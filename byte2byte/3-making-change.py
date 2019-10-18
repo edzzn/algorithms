8af7bbc8cf16da82b36660d36548330df1d0e605
Edisson Reinozo <edisson350@gmail.com>
2019-10-18 00:13:50 -0500

    making change

---
+++ byte2byte/3-making-change.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+def make_change(x):
+    coins = [25, 10, 5, 1]
+    coin_counter = 0
+    for coin in coins:
+        while x >= coin:
+            x -= coin
+            coin_counter += 1
+    return coin_counter
+
+
+if __name__ == '__main__':
+    x = 167
+    result = make_change(x)
+    print(result)
