42a89a492839e567391697b77769d3b6859e01d1
Edisson Reinozo <edisson350@gmail.com>
2019-10-19 14:48:59 -0500

    Better solution

--- leetcode/jewels-and-stones.py
+++ leetcode/jewels-and-stones.py
@@ -16,13 +16,25 @@
 
 #     return counter
 
+# def numJewelsInStones(J: str, S: str) -> int:
+#     counter = 0
+
+#     for jewel in J:
+#         for stone in S:
+#             if jewel == stone:
+#                 counter += 1
+
+#     return counter
+
+# Best solution
 def numJewelsInStones(J: str, S: str) -> int:
     counter = 0
+    my_dict = dict()
+    for stone in S:
+        my_dict[stone] = my_dict.get(stone, 0) + 1
 
     for jewel in J:
-        for stone in S:
-            if jewel == stone:
-                counter += 1
+        counter += my_dict.get(jewel, 0)
 
     return counter
 
